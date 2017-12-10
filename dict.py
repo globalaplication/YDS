2ba583cd3a7c9fa56623e2d14f07e1eab354c22f
Nuri Özdaş <32783197+globalaplication@users.noreply.github.com>
2017-12-10 23:35:42 +0300

    Update dict.py

--- dict.py
+++ dict.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
-file = open('dict.data')
+file = open('data.mmsql')
 source = file.read()
 source = source.splitlines()
-ing = [ing.split(':')[0] for ing in source]
+dict = [dict.split(':')[0:] for dict in source]
 file.close()
-def dict(ingilizce):
-	dict = {}
-	for search in ing:
-		if (len(search) is len(ingilizce) and search == ingilizce):
-			dict[search] = source[ing.index(search)].split(':')[1:][0]
-			break
-	return dict
-
+def sozluk(string):
+	for test in dict:
+		for extend in test:
+			extend = extend + extend
+		if string in extend:
+			return test[0]
+		if test[0] == string:
+			return test[1:]
